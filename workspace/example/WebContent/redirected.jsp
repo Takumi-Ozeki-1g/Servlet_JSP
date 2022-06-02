c196bb5a7e44b03bd33acb052850ed41ea9cea13
Takumi-Ozeki-1g <sb22723s@st.omu.ac.jp>
2022-06-03 00:40:09 +0900

    Study Redirect and Forward

---
+++ workspace/example/WebContent/redirected.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>リダイレクトされました</title>
+</head>
+<body>
+リダイレクトされたということは奇数だね
+</body>
+</html>
---
+++ workspace/example/src/ex/Ex62Servlet.java
@@ -0,0 +1,28 @@
+package ex;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ex62
+ */
+@WebServlet("/ex62")
+public class Ex62Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int rand = (int) (Math.random() * 10);
+		if(rand % 2 == 1) {
+			response.sendRedirect("/example/redirected.jsp");
+		} else {
+			RequestDispatcher d = request.getRequestDispatcher("/forwarded.jsp");
+			d.forward(request, response);
+		}
+	}
+}
